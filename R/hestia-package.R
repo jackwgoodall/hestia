1c9fe21f86e9527887125696a874652ec2be9927
Smith <cpsmit@AD.UNC.EDU>
2026-01-26 09:55:16 -0500

    additional package files

--- R/hestia-package.R
+++ R/hestia-package.R
@@ -1,9 +1,17 @@
-#' @keywords internal
+#' The 'hestia' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @name hestia-package
+#' @aliases hestia
+#' @useDynLib hestia, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.7. https://mc-stan.org
+#'
 "_PACKAGE"
-
-## usethis namespace: start
-#' @import rlang
-#' @importFrom glue glue
-#' @importFrom lifecycle deprecated
-## usethis namespace: end
-NULL
