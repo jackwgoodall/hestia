6af982aa45066469619ec1ab5adbd49ba2f60b35
Jack Goodall <116029081+jackwgoodall@users.noreply.github.com>
2026-04-20 14:18:57 +0000

    Update simulation.sh

--- JG_HPC/simulation.sh
+++ JG_HPC/simulation.sh
@@ -3,10 +3,9 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=jack.goodall@lshtm.ac.uk # Where to send mail
 #SBATCH --nodes=1                     # Run all processes on a single node
-#SBATCH --ntasks=4                   # Run a single task
-#SBATCH --nodes=1                     # Keeps the job running on a single node.
-#SBATCH --mem=1gb                     # Total memory limit
-#SBATCH --time=00:05:00               # Time limit hrs:min:sec
+#SBATCH --ntasks=1                 # Run a single task
+#SBATCH --mem=32gb                     # Total memory limit
+#SBATCH --time=02:00:00               # Time limit hrs:min:sec
 #SBATCH --output=parallel_%j.log      # Standard output and error log
 date;hostname;pwd
 
